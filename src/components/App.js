aad0dda132334b409fc4e5fe13e31b4abe31916a
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 14:52:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-
 import React from "react";
-import './../styles/App.css';
+import DataFetcher from "./DataFetcher";
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <DataFetcher />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
